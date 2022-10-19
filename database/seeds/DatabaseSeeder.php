d7c812958a00ebd33270192bc3972d3e2d4010b5
Omar Rivas <orivas@techno-screen.com>
2022-10-19 16:13:44 -0600

    Se agrega data por default para movimientos.

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,11 +12,35 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         DB::table('tiposmovimientos')->insert([
-            'nombre' => "Entrada",
+            'nombre' => 'Entrada',
         ]);
 
         DB::table('tiposmovimientos')->insert([
-            'nombre' => "Salida",
+            'nombre' => 'Salida',
+        ]);
+
+        DB::table('movimientos')->insert([
+            'idTipo' => 1,
+            'tipo' => 1,
+            'monto' => 240,
+            'fecha' => '2022-10-19 16:00:00.000000',
+            'Adjunto' => 'foto1.jpg',
+        ]);
+        
+        DB::table('movimientos')->insert([
+            'idTipo' => 2,
+            'tipo' => 0,
+            'monto' => 180,
+            'fecha' => '2022-10-19 16:20:00.000000',
+            'Adjunto' => 'foto2.jpg',
+        ]);
+
+        DB::table('movimientos')->insert([
+            'idTipo' => 1,
+            'tipo' => 1,
+            'monto' => 160,
+            'fecha' => '2022-10-19 16:15:00.000000',
+            'Adjunto' => 'foto3.jpg',
         ]);
     }
 }
